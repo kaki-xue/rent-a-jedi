4969a1ca9ed9e68fb76587b54b96914cca42a2b5
Tom Hill <tom.hill991@gmail.com>
2019-11-27 14:18:43 +0800

    added route

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,9 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resources :users, only: [:index, :show, :create, :destroy]
+      resources :users, only: [:index, :show, :create, :destroy] do
+        resources :bookings, only: [:index, :show]
+      end
     end
   end
 
